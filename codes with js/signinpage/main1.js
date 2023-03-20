40217bfcd6ac341112fbbf7658268f1d6b27f521
Shrishvesh Reddy <=>
2023-03-20 18:29:12 +0530

    Initial Commit

---
+++ codes with js/signinpage/main1.js
@@ -0,0 +1,5 @@
+let my_url="https://www.youtube.com/watch?v=lqwdD2ivIbM&list=PLpwngcHZlPaczK3fy-v0YifQyjFbtLHDS";
+document.getElementsByClassName("full").onclick= 
+function(){
+    window.location.replace(my_url)
+}
